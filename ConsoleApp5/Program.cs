e051615d51328a61d0f20216d55a9add8f867cf4
JulianaMistron <jumistron@yahoo.com.br>
2024-04-20 12:40:49 -0300

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,20 @@
+﻿double lado1, lado2, lado3;
+
+Console.WriteLine("Digite o lado 1:");
+lado1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Digite o lado 2:");
+lado2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Digite o lado 3:");
+lado3 = Convert.ToDouble(Console.ReadLine());
+
+{
+    if (lado1 == lado2 && lado2 == lado3)
+        Console.WriteLine("Triângulo Equilátero");
+
+    else if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3)
+        Console.WriteLine("Triângulo Isósceles");
+    else
+        Console.WriteLine("Triângulo Escaleno");
+}
